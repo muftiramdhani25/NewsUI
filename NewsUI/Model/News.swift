6a1337c439775d3f064ca416bc2e54943424d4d1
muhammad mufti ramdhani <muftiramdhani25@gmail.com>
2021-02-08 13:50:49 +0700

    finished project

---
+++ NewsUI/Model/News.swift
@@ -0,0 +1,15 @@
+//
+//  News.swift
+//  NewsUI
+//
+//  Created by abdullah irianda on 08/02/21.
+//
+
+import SwiftUI
+
+struct News : Identifiable {
+    var id = UUID()
+    var title : String
+    var image : String
+    var description : String
+}
